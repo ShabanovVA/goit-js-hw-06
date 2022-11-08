37ca2737b8ef5ce8c7baca6f52f113460d6e029b
ShabanovVA <1514768zp@gmail.com>
2022-11-08 16:46:44 +0200

    Завдання 9.

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,12 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const buttonEl = document.querySelector(".change-color");
+const textColorEl = document.querySelector(".color");
+
+function onRandomHexColor () {
+  textColorEl.style.backgroundColor = getRandomHexColor();
+}
+
+  buttonEl.addEventListener('click', onRandomHexColor);
