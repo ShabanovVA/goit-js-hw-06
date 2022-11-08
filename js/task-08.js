4e3b6430d54e5355122a959e1e5514b0abc669f6
ShabanovVA <1514768zp@gmail.com>
2022-11-08 11:38:46 +0200

    Завдання 8.

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,17 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", onFormSubmit);
+
+function onFormSubmit(event) {
+    event.preventDefault();
+
+    const formElements = event.currentTarget.elements;
+    const mail = formElements.email.value;
+    const password = formElements.password.value;
+
+    if (mail === "" || password === "") {
+        return alert('Усі поля мають бути заповнені!');
+    }
+        console.log(`Email: ${mail}, Password: ${ password}`);
+        event.currentTarget.reset();
+}
